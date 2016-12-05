5f9796e74236349653f751b1f680e1319bdf7578
kunggewan <kunggewan@hotmail.com>
2016-12-05 11:10:32 +0800

    home

---
+++ AspNetTech/Controllers/ApiTestController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AspNetTech.Controllers
+{
+    public class ApiTestController : Controller
+    {
+        // GET: ApiTest
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult TestGuestV1()
+        {
+            return View();
+        }
+
+        public ActionResult TestGuestV2()
+        {
+            return View();
+        }
+    }
+}
