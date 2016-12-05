c42816585f621ea3c1faccf59f119a6209da6ca1
kunggewan <kunzhongzhong@163.com>
2016-12-05 11:09:21 +0800

    office

---
+++ AspNetTech/Controllers/NetApiTestController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AspNetTech.Controllers
+{
+    public class NetApiTestController : Controller
+    {
+        // GET: NetApiTest
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        public ActionResult PostTest()
+        {
+            return View();
+        }
+
+        public ActionResult GuestDemo()
+        {
+            return View();
+        }
+
+
+        public ActionResult TestApiProj()
+        {
+            return View();
+        }
+    }
+}
